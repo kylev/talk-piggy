e36aeb333d400d2d5a955c07703d7e9c8058c852
Kyle VanderBeek <kylev@kylev.com>
2019-04-05 13:29:51 -0700

    Shim speaker list.

---
+++ src/components/SpeakerList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Thinger</p>
+    <ul>
+      <li v-for="speaker in speakers" :key="speaker.name">{{ speaker.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    speakers() {
+      return this.$store.state.speakers;
+    }
+  }
+};
+</script>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    speakers: [{ name: "Steven" }, { name: "Nick" }]
+  },
   mutations: {},
   actions: {}
 });
