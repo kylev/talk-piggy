7f3488f800f005f0bdb78459d114dfbf542ad2ea
Kyle VanderBeek <kylev@kylev.com>
2019-04-07 18:57:47 -0700

    Get rid of HelloWorld component.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
-  <div id="app">
+  <div class="container" id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <SpeakerList />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import SpeakerList from "./components/SpeakerList.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld,
     SpeakerList
   }
 };
