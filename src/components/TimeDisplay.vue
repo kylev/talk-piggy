b6055a302f15bc4f4ccae118cb3a0048d01b198c
Kyle VanderBeek <kylev@kylev.com>
2019-04-09 15:38:51 -0700

    Add time elapsed display.

---
+++ src/components/TimeDisplay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span v-if="hours">{{ hours }}:</span>
+    <span>{{ minutes }}:</span>
+    <span>{{ seconds }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["time"],
+  computed: {
+    hours() {
+      const h = Math.floor(this.time / 3600);
+      return h > 0 ? h : null;
+    },
+    minutes() {
+      return (Math.floor(this.time / 60) % 60).toString().padStart(2, "0");
+    },
+    seconds() {
+      return (this.time % 60).toString().padStart(2, "0");
+    }
+  }
+};
+</script>
