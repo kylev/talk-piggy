8e2ab3ca0a177c241c97775f4bf5c782cecdc9d2
Kyle VanderBeek <kylev@kylev.com>
2019-04-09 13:18:49 -0700

    Start conversion to bootstrap.

--- src/components/SpeakerList.vue
+++ src/components/SpeakerList.vue
@@ -1,11 +1,9 @@
 <template>
-  <section class="tile is-ancestor">
-    <div class="tile is-parent" v-for="speaker in speakers" :key="speaker.name">
-      <article class="tile is-child box">
-        <p class="title">{{ speaker.name }}</p>
-      </article>
-    </div>
-  </section>
+  <div>
+    <b-card v-bind:title="speaker.name" v-for="speaker in speakers" :key="speaker.name">
+      <b-button>Talking</b-button>
+    </b-card>
+  </div>
 </template>
 
 <script>
