63a7b7ca2348d19626704e3fea6207ef00a4c1b9
Kyle VanderBeek <kylev@kylev.com>
2019-04-09 13:19:12 -0700

    Trivial timer control.

--- src/components/TimerControl.vue
+++ src/components/TimerControl.vue
@@ -1,9 +1,21 @@
 <template>
   <section>
-    <button class="button is-primary">Start / Stop</button>
+    <b-button v-if="!running" v-on:click="startTimer">Start</b-button>
+    <b-button v-if="running" v-on:click="stopTimer">Stop</b-button>
   </section>
 </template>
 
 <script>
-export default {};
+import { mapActions } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions(["startTimer", "stopTimer"])
+  },
+  computed: {
+    running() {
+      return this.$store.state.tickTimer !== null;
+    }
+  }
+};
 </script>
--- src/store.js
+++ src/store.js
@@ -1,18 +1,29 @@
-import Buefy from "buefy";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
-Vue.use(Buefy);
 
 export default new Vuex.Store({
   state: {
-    speakers: [{ name: "Steven" }, { name: "Nick" }]
+    speakers: [{ name: "Steven" }, { name: "Nick" }],
+    tickSeconds: 0,
+    tickTimer: null
   },
   mutations: {
     addSpeaker(state, name) {
       state.speakers.push({ name });
+    },
+    incrementTick(state) {
+      state.tickSeconds++;
     }
   },
-  actions: {}
+  actions: {
+    startTimer({ commit, state }) {
+      state.tickTimer = setInterval(() => commit("incrementTick"), 1000);
+    },
+    stopTimer({ state }) {
+      clearInterval(state.tickTimer);
+      state.tickTimer = null;
+    }
+  }
 });
