849e439200ecc6437189dfaeaf109433cae43412
Kyle VanderBeek <kylev@kylev.com>
2019-04-10 00:18:41 -0700

    Hook up modal user add.

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,10 @@
     <TopBar />
     <MainPanel />
     <SpeakerList />
-    <AddSpeaker />
   </div>
 </template>
 
 <script>
-import AddSpeaker from "./components/AddSpeaker.vue";
 import MainPanel from "./components/MainPanel.vue";
 import SpeakerList from "./components/SpeakerList.vue";
 import TopBar from "./components/TopBar.vue";
@@ -16,7 +14,6 @@ import TopBar from "./components/TopBar.vue";
 export default {
   name: "app",
   components: {
-    AddSpeaker,
     MainPanel,
     SpeakerList,
     TopBar
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,21 +1,47 @@
 <template>
-  <b-navbar type="dark" variant="info" sticky="true">
+  <b-navbar type="dark" variant="info" sticky>
     <b-navbar-brand href="#">Talk Piggy</b-navbar-brand>
     <b-navbar-nav>
       <b-nav-item href="#"><TimerControl /></b-nav-item>
     </b-navbar-nav>
-    <b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
       <b-nav-item-dropdown text="Menu" right>
-        <b-dropdown-item href="#">Thingy</b-dropdown-item>
+        <b-dropdown-item href="#" v-b-modal.add-speaker-modal>
+          Add Speaker
+        </b-dropdown-item>
       </b-nav-item-dropdown>
     </b-navbar-nav>
+    <b-modal ref="modal" id="add-speaker-modal" @ok="handleOk">
+      <b-form @submit.stop.prevent="handleSubmit">
+        <b-form-input placeholder="Joe Schmo" v-model.trim="name" />
+      </b-form>
+    </b-modal>
   </b-navbar>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
+//import AddSpeaker from "./AddSpeaker.vue";
 import TimerControl from "./TimerControl.vue";
 
 export default {
-  components: { TimerControl }
+  components: { TimerControl },
+  data() {
+    return {
+      name: ""
+    };
+  },
+  methods: {
+    ...mapMutations(["addSpeaker"]),
+    handleOk() {
+      this.addSpeaker(this.name);
+      this.name = "";
+    },
+    handleSubmit() {
+      this.handleOk(this.name);
+      this.$refs.modal.hide();
+    }
+  }
 };
 </script>
