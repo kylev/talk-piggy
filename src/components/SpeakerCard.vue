ffbe425db26fe5f0369a7aff801a4b9c6cf48f30
Kyle VanderBeek <kylev@kylev.com>
2019-04-11 16:15:48 -0700

    Make speaker cards reactive, lose the talk button.

--- src/components/SpeakerCard.vue
+++ src/components/SpeakerCard.vue
@@ -1,7 +1,11 @@
 <template>
-  <b-card>
-    <div slot="header">
-      {{ speaker.name }}
+  <b-card
+    @click="$emit('toggle')"
+    @mouseenter="hover = true"
+    @mouseleave="hover = false"
+    :body-bg-variant="color"
+  >
+    <b-card-title>
       <b-button
         @click="$emit('remove')"
         size="sm"
@@ -9,17 +13,11 @@
         class="float-right"
         >X</b-button
       >
-    </div>
+      {{ speaker.name }}
+    </b-card-title>
     <b-card-text>
       <TimeDisplay v-bind:time="speaker.speakingSeconds" />
     </b-card-text>
-    <b-button
-      v-on:click="$emit('toggle')"
-      v-bind:pressed="speaker.isSpeaking"
-      variant="outline-primary"
-    >
-      Talking
-    </b-button>
   </b-card>
 </template>
 
@@ -30,6 +28,17 @@ export default {
   props: ["speaker"],
   components: {
     TimeDisplay
+  },
+  data() {
+    return {
+      hover: false
+    };
+  },
+  computed: {
+    color() {
+      if (this.speaker.isSpeaking) return "primary";
+      else return this.hover ? "secondary" : "";
+    }
   }
 };
 </script>
