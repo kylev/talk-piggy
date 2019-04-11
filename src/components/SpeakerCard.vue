e3d18487847145947ee8f9c5b2700b51391efd3a
Kyle VanderBeek <kylev@kylev.com>
2019-04-11 16:37:05 -0700

    Move "remove" into a menu to make it less likely to single-click.

--- src/components/SpeakerCard.vue
+++ src/components/SpeakerCard.vue
@@ -6,14 +6,12 @@
     :body-bg-variant="color"
   >
     <b-card-title>
-      <b-button
-        @click="$emit('remove')"
-        size="sm"
-        variant="outline-danger"
-        class="float-right"
-        >X</b-button
-      >
       {{ speaker.name }}
+      <b-dropdown right :variant="color || 'white'" class="float-right">
+        <b-dropdown-item @click="$emit('remove')">
+          Remove
+        </b-dropdown-item>
+      </b-dropdown>
     </b-card-title>
     <b-card-text>
       <TimeDisplay v-bind:time="speaker.speakingSeconds" />
@@ -37,7 +35,7 @@ export default {
   computed: {
     color() {
       if (this.speaker.isSpeaking) return "primary";
-      else return this.hover ? "secondary" : "";
+      else return this.hover ? "light" : null;
     }
   }
 };
