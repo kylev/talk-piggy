fea847d810718a332e2de94e8c7001edb9d7a331
Kyle VanderBeek <kylev@kylev.com>
2019-04-11 17:18:25 -0700

    Put top var in a container to get the items off the edges.

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,31 +1,33 @@
 <template>
   <b-navbar type="dark" variant="info" sticky>
-    <b-navbar-brand href="#">Talk Piggy</b-navbar-brand>
-    <b-navbar-nav>
-      <b-nav-item href="#"><TimerControl /></b-nav-item>
-    </b-navbar-nav>
-    <b-navbar-nav class="ml-auto">
-      <b-nav-item-dropdown text="Menu" right>
-        <b-dropdown-item href="#" v-b-modal.add-speaker-modal>
-          Add Piggy
-        </b-dropdown-item>
-      </b-nav-item-dropdown>
-    </b-navbar-nav>
-    <b-modal
-      ref="modal"
-      id="add-speaker-modal"
-      @ok="handleOk"
-      @shown="focusInput"
-      title="Add Piggy"
-    >
-      <b-form @submit.stop.prevent="handleSubmit">
-        <b-form-input
-          v-model.trim="name"
-          ref="name-field"
-          placeholder="Joe Schmo"
-        />
-      </b-form>
-    </b-modal>
+    <b-container>
+      <b-navbar-brand href="#">Talk Piggy</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="#"><TimerControl /></b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Menu" right>
+          <b-dropdown-item href="#" v-b-modal.add-speaker-modal>
+            Add Piggy
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+      <b-modal
+        ref="modal"
+        id="add-speaker-modal"
+        @ok="handleOk"
+        @shown="focusInput"
+        title="Add Piggy"
+      >
+        <b-form @submit.stop.prevent="handleSubmit">
+          <b-form-input
+            v-model.trim="name"
+            ref="name-field"
+            placeholder="Joe Schmo"
+          />
+        </b-form>
+      </b-modal>
+    </b-container>
   </b-navbar>
 </template>
 
