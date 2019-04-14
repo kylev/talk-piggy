c3972af5f4cc2f14d0662c6f7ce1f91b491046d8
Kyle VanderBeek <kylev@kylev.com>
2019-04-13 18:22:56 -0700

    Let GA know we've been viewed.

--- src/main.js
+++ src/main.js
@@ -19,5 +19,8 @@ Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
+  mounted() {
+    this.$ga.page(process.env.BASE_URL);
+  }
 }).$mount("#app");
