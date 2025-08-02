23836fa80a81a2ce4de56690b626494178b5165c
Kyle VanderBeek <kylev@kylev.com>
2025-08-02 12:06:53 -0700

    Auto register bootstrap components for templates.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -8,7 +10,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools()
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
+    vueDevTools(),
   ],
   resolve: {
     alias: {
