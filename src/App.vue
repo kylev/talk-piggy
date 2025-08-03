d244d0f54f1e28895aca58f0b97350bc16469dd3
Kyle VanderBeek <kylev@kylev.com>
2025-08-03 07:12:02 -0700

    Undo intentional lint sin.

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@
 import MainPanel from './components/MainPanel.vue'
 import SpeakerList from './components/SpeakerList.vue'
 import TopBar from './components/TopBar.vue'
-import { defineAsyncComponent } from 'vue'
 
 export default {
   name: 'app',
